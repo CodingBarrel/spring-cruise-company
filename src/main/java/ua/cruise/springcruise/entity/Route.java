4e97151a7cf094982b3cd6101b7095885dd0e3b4
Vladyslav Kucher <ku_vlad@ukr.net>
2022-06-28 10:15:37 +0300

    Add route entity and repository

---
+++ src/main/java/ua/cruise/springcruise/entity/Route.java
@@ -0,0 +1,24 @@
+package ua.cruise.springcruise.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Route {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+}
---
+++ src/main/java/ua/cruise/springcruise/repository/RouteRepository.java
@@ -0,0 +1,7 @@
+package ua.cruise.springcruise.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ua.cruise.springcruise.entity.Route;
+
+public interface RouteRepository extends JpaRepository<Route, Long> {
+}
