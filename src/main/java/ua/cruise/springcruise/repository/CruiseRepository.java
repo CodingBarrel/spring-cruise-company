fa3af4c767b01b180c04b645a638c7925ba61987
Vladyslav Kucher <ku_vlad@ukr.net>
2022-07-09 16:44:30 +0300

    Bug fix

--- src/main/java/ua/cruise/springcruise/repository/CruiseRepository.java
+++ src/main/java/ua/cruise/springcruise/repository/CruiseRepository.java
@@ -1,12 +1,11 @@
 package ua.cruise.springcruise.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import ua.cruise.springcruise.entity.Cruise;
 
 import java.util.Optional;
 
-public interface CruiseRepository extends JpaRepository<Cruise, Long> {
+public interface CruiseRepository extends JpaRepository<Cruise, Long>, JpaSpecificationExecutor<Cruise> {
     Optional<Cruise> findByName(String name);
-
-
 }
